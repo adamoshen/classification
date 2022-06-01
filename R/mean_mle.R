e23b21e66edf55ca53ae6348ae0e4318c338d63e
adamoshen <46703463+adamoshen@users.noreply.github.com>
2022-05-31 23:28:37 -0400

    Add documentation for `mean_mle.R`

--- R/mean_mle.R
+++ R/mean_mle.R
@@ -1,3 +1,12 @@
+#' Compute maximum likelihood estimate of population mean
+#'
+#' For data from a multivariate normal distribution, the maximum likelihood estimate of the
+#' population mean is computed as the sample means of the individual dimensions.
+#'
+#' @param x A matrix of dimension \eqn{d} by \eqn{n}, where \eqn{d} is the number of dimensions and
+#' \eqn{n} is the sample size.
+#' @return A matrix of dimension \eqn{d} by \eqn{1}.
+#' @export
 mean_mle <- function(x) {
   n <- ncol(x)
 
