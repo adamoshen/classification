2de06b70dc2015dfc44262540da6cd30f3455438
adamoshen <46703463+adamoshen@users.noreply.github.com>
2022-06-14 23:32:02 -0400

    Add imports

--- R/classification-package.R
+++ R/classification-package.R
@@ -2,6 +2,15 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr across
+#' @importFrom dplyr distinct
+#' @importFrom dplyr if_else
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr relocate
+#' @importFrom dplyr select
 #' @importFrom magrittr %>%
+#' @importFrom purrr pluck
+#' @importFrom tidyselect everything
 ## usethis namespace: end
 NULL
